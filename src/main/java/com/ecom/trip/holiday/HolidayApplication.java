816ccc21e754e06f8abef7bf65aa669b1d5ecfb3
VasanthaPrabhaS <slvbaviyasri@gmail.com>
2024-10-27 14:03:57 +0530

    Holiday 0.6 Swagger UI springdoc-openapi-starter-webmvc-ui

--- src/main/java/com/ecom/trip/holiday/HolidayApplication.java
+++ src/main/java/com/ecom/trip/holiday/HolidayApplication.java
@@ -2,7 +2,6 @@ package com.ecom.trip.holiday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class HolidayApplication {
 
--- src/main/java/com/ecom/trip/holiday/bo/Product.java
+++ src/main/java/com/ecom/trip/holiday/bo/Product.java
@@ -2,6 +2,7 @@ package com.ecom.trip.holiday.bo;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
